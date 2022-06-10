43d1b29df16a78ef5696b20f1f910a39974958c5
manoelalexandr <manoelalexandr@gmail.com>
2022-06-10 16:46:12 -0300

    Fix DB bug

--- src/services/alunoService.js
+++ src/services/alunoService.js
@@ -5,7 +5,7 @@ module.exports = {
     findAll: () => {
         return new Promise((accept, reject) => {
             
-            db.query('SELECT * FROM ALUNOS', (error, results) => {
+            db.query('SELECT * FROM alunos', (error, results) => {
                 if(error) { reject(error); return;}
                 accept(results);
             });
@@ -21,17 +21,17 @@ module.exports = {
                     accept(results[0]);                    
                 }else{
                     accept(false);
-                }
+                }     
             });
         });
     },
 
     insertAluno: (name,email, password, tipo) => {
         return new Promise((accept, reject) => {
-
+            
             db.query(`INSERT INTO alunos (name, email, password, fkTipo) VALUES ('${name}','${email}','${password}',${tipo})`,(error, results) =>{
                 if(error) { reject(error); return;}
-                accept(results.insertId);
+                accept(results.insertId);   
                 }
             );
         });
